36f525fca7354d8fa035a6b23acc319c760edff1
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2024-03-06 23:14:33 -0500

    Insure final slash in env dir

--- src/BlueRipple/Data/CPS_VS_Path.hs
+++ src/BlueRipple/Data/CPS_VS_Path.hs
@@ -8,12 +8,13 @@ module BlueRipple.Data.CPS_VS_Path
   )
 where
 
+import qualified BlueRipple.Data.CachingCore as BRC
 import qualified Frames.Streamly.TH as F
 import qualified Frames.Streamly.ColumnUniverse         as FCU
 import qualified Language.Haskell.TH.Env as Env
 
 dataDir :: [Char]
-dataDir = fromMaybe "../bigData/IPUMS/" $$(Env.envQ "BR_CPS_VS_DATA_DIR")
+dataDir = fromMaybe "../bigData/IPUMS/" $ fmap toString $ $$(Env.envQ "BR_CPS_VS_DATA_DIR") >>= BRC.insureFinalSlash . toText
 
 cpsVoterPUMSCSV :: FilePath
 cpsVoterPUMSCSV = dataDir ++ "CPS_Voting_2006to2022.csv"
